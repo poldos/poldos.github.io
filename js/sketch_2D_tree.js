673fcc565811407e98fd4d62458196807c6c97a4
poldo <84807930+poldos@users.noreply.github.com>
2023-05-25 18:01:58 +0200

    horizontal hamburger menu

---
+++ js/sketch_2D_tree.js
@@ -0,0 +1,142 @@
+// Global variables
+let canvasFrame;
+let canvasWidth;
+let canvasHeight;
+let canvasX;
+let canvasY;
+let menuDropdown;
+
+let canvasColorPicker;
+let woodColorPicker;
+let leafColorPicker;
+
+let rSeed;
+
+let newTreeButton;
+
+// setup Sketch UI
+function setup() {
+  canvasWidth = min(windowWidth, 750);
+  canvasHeight = min(windowHeight, 1000);
+
+  canvasFrame = createCanvas(canvasWidth, canvasHeight);
+
+  canvasX = (windowWidth - canvasWidth) / 2;
+  canvasY = (windowHeight - canvasHeight + 125) / 2;
+  canvasFrame.position(canvasX, canvasY);
+  console.log(canvasX, canvasY);
+
+  // set white background
+  background(255);
+
+  // to align to menu dropdown
+  menuDropdown = document.getElementById("m1").getBoundingClientRect();
+
+  // create colour pickers (from cache or default if nothing in cache)
+  canvasColorPicker = createColorPicker(localStorage['canvas'] || '#FFFFFF');
+  canvasColorPicker.position(menuDropdown.x, canvasY);
+  woodColorPicker = createColorPicker(localStorage['wood'] || '#000000');
+  woodColorPicker.position(menuDropdown.x, canvasY + 40);
+  leafColorPicker = createColorPicker(localStorage['leaf'] || '#888888');
+  leafColorPicker.position(menuDropdown.x, canvasY + 80);
+
+  // create button for page reload / new tree
+  newTreeButton = createButton('new Tree');
+  newTreeButton.style('font-color: blue;');
+  newTreeButton.position(menuDropdown.x, canvasY + 120);
+  newTreeButton.size(50,25);
+  newTreeButton.mousePressed(reloadPage);
+  angleMode(DEGREES);
+  noLoop();
+  initSeed();
+}
+
+// initialise seed to use until next window reload
+function initSeed() {
+  rSeed = random(1, 100);
+}
+
+// reload page on user request
+function reloadPage() {
+  location.reload();
+}
+
+// update colours on user request
+function updateColors() {
+  localStorage['canvas'] = canvasColorPicker.value();
+  localStorage['wood'] = woodColorPicker.value(); 
+  localStorage['leaf'] = leafColorPicker.value();
+  redraw();
+}
+
+function draw() {
+  // set background, add colour pickers to page and update colours per user input
+  //translate(width/2, height/2);
+  background(canvasColorPicker.value());
+  noStroke();
+  fill(0);
+  
+  //textStyle(BOLD);
+  //text("Background", menuDropdown.x, menuDropdown.y + menuDropdown.height + 10);
+  canvasColorPicker.input(updateColors);
+  //text("Wood", 250, menuDropdown.y + menuDropdown.height + 10);
+  woodColorPicker.input(updateColors);
+  //text("Leaves", 450, menuDropdown.y + menuDropdown.height + 10);
+  leafColorPicker.input(updateColors);
+  //text("new Tree", 650, menuDropdown.y + menuDropdown.height + 10);
+
+  // start drawing
+  randomSeed(rSeed); // same random seed for each redraw
+  translate(canvasWidth/2, canvasHeight/2 + 250);
+  branch(100);
+}
+
+// setBackgroundColor()
+
+function branch(len) {
+  push()
+
+// Generate trunc and branches
+  if(len > 10) {
+    // branches
+    
+    strokeWeight(map(len, 10, 100, 1, 15));
+    stroke(woodColorPicker.value());//brown branches
+    line(0, 0, 0, -len);
+    translate(0, -len);
+    rotate(random(-20, -30));
+    branch(random(0.7, 0.9)*len);
+    rotate(random(50,60));
+    branch(random(0.7, 0.9)*len);
+  } else {
+    // leaves
+    let r = 88 + random(-20, 20);
+    let g = 2 + random(-20, 20);
+    let b = 170 + random(-20, 20);
+
+    fill(leafColorPicker.value());//coloured leaves
+    noStroke();
+
+    // generate leaves
+    //left half
+    beginShape();
+    for (let i = 45; i < 135; i++) {
+      let leafRadius = 10;
+      let x = leafRadius * cos(i);
+      let y = leafRadius * sin(i);
+      vertex(x, y);
+    }
+    endShape();
+    //right half
+    beginShape();
+    for (let i = 135; i > 40; i--) {
+      let leafRadius = 10;
+      let x = leafRadius * cos(i);
+      let y = leafRadius * sin(-i) + 20;
+      vertex(x, y);
+    }
+    endShape();
+
+  }
+  pop();
+}
