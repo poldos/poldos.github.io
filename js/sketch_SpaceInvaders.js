92e503f7084d447ace0dbf3101120fda46cb0e21
poldo <84807930+poldos@users.noreply.github.com>
2023-05-28 17:37:22 +0200

    publish SpaceInvader page only

--- js/sketch_SpaceInvaders.js
+++ js/sketch_SpaceInvaders.js
@@ -117,12 +117,13 @@ function setup() {
   // use degrees as angle unit
   angleMode(DEGREES);
   // to align legend and canvas to horizontal menu, get its coordinates
-  menu = document.getElementById("m1").getBoundingClientRect();
+  //menu = document.getElementById("m1").getBoundingClientRect();
   // create canvas
-  canvasX = menu.x;// + 140;
-  canvasY = menu.y + menu.height + 10;
-  canvasWidth = menu.width;// - 140;
+  canvasY = 0;//menu.y + menu.height + 10;
+  canvasWidth = min(960, windowWidth);//menu.width;// - 140;
   canvasHeight = min(windowHeight - canvasY - 10, 600);
+  canvasX = (windowWidth - canvasWidth) / 2;//menu.x;// + 140;
+
   canvasFrame = createCanvas(canvasWidth, canvasHeight);
   canvasFrame.position(canvasX, canvasY);
   // set white background
