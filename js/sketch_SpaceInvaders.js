0575b04d2c0a218ca76b125ba7a83e437d1dc2bc
poldo <84807930+poldos@users.noreply.github.com>
2023-05-30 14:53:08 +0200

    full github not just spaceinvaders

--- js/sketch_SpaceInvaders.js
+++ js/sketch_SpaceInvaders.js
@@ -11,12 +11,12 @@ let imgs = [];
 let imageNu;
 
 let spaceInvaders = [];
-let spaceInvaderRadius = 60;
+let spaceInvaderRadius = 60;//40;
 let spaceInvaderRowHeight = 40;
-let spaceInvaderColSpacing = 120;
+let spaceInvaderColSpacing = 120;//80;
 let spaceInvaderRows = 6;
 let spaceInvaderSpeed = 20;
-let spaceInvaderTickRate = 10; // move only every tickRate frames
+let spaceInvaderTickRate = 10;// move only every tickRate frames
 let spaceInvaderAngle = 5; // head rotation angle
 
 let laserRays = [];
@@ -30,20 +30,27 @@ let newGameButton;
 // CLASSES
 
 class SpaceInvader {
-  constructor(img, x, y, angle, speed, shotInterval, hits, health) {
+  constructor(img, x, y, radius, angle, speed, shotInterval, hits, health) {//, r, g, b) {
     this.img = img;
     this.x = x;
     this.y = y;
+    this.radius = radius;
     this.angle = angle;
-    this.xSpeed = 0;
-    this.ySpeed = speed / 100;
+    this.xSpeed = 0;//(speed * cos(angle))/100;
+    this.ySpeed = speed / 100; //(speed * sin(angle))/100;
     this.shotInterval = shotInterval; // number of frames btw shots
     this.hits = hits; // after 5 hits, 
     this.health = health; // health goes down 1
+    //this.r = r;
+    //this.g = g;
+    //this.b = b;
     this.tick = 0;
   }
   // display image rotated +/- spaceInvaderAngle at each step
   show() {
+    //stroke(this.r, this.g, this.b);
+    //fill(this.r, this.g, this.b);
+    //circle(this.x, this.y, this.radius);
     push();
     translate(this.x + spaceInvaderRadius/2, this.y + spaceInvaderRadius/2);
     rotate(this.angle);
@@ -61,7 +68,7 @@ class SpaceInvader {
     }
   }
   // if hit, reduce health counter
-  updateHealth() {
+    updateHealth() {
     if (this.hits != 0 && this.hits % 5 == 0) {
       this.health--;
     }
@@ -78,29 +85,30 @@ class LaserRay {
     this.b = b;
     this.xSpeed = speed * cos(angle);
     this.ySpeed = speed * sin(angle);
+    //this.killBool = false;
   }
-  // display ray
   show() {
     stroke(this.r, this.g, this.b);
     fill(this.r, this.g, this.b);
     rect(this.x, this.y, (this.length * cos(this.angle)+2), (this.length * sin(this.angle))+2);
   }
-  // move ray upward
   move() {
     this.x += this.xSpeed;
     this.y += this.ySpeed;
   }
-  // if spaceInvader is within distance, hit it !
   hitSpaceInvader() {
     for (let i = 0; i < spaceInvaders.length; i++) {
       if (Math.abs(this.x - spaceInvaders[i].x) < rayHitPrecision  && Math.abs(this.y - spaceInvaders[i].y) < rayHitPrecision) {
+
         noStroke();
         fill(this.r, this.g, this.b);
         circle(this.x, this.y, this.radius * 3);
+
         spaceInvaders[i].hits += 1;
         spaceInvaders[i].updateHealth();
         if (spaceInvaders[i].health == 0) {
           spaceInvaders.splice(i, 1);
+          //this.killBool = true;
         }
       }
     }
@@ -117,11 +125,13 @@ function preload() {
 function setup() {
   // use degrees as angle unit
   angleMode(DEGREES);
+  // to align legend and canvas to horizontal menu, get its coordinates
+  menu = document.getElementById("m1").getBoundingClientRect();
   // create canvas
-  canvasWidth = min(1280,windowWidth - 50);
-  canvasHeight = windowHeight - 50;
-  canvasX = (windowWidth - canvasWidth) / 2;
-  canvasY = 0;
+  canvasX = menu.x;// + 140;
+  canvasY = menu.y + menu.height + 10;
+  canvasWidth = menu.width;// - 140;
+  canvasHeight = windowHeight - 50;//min(windowHeight - canvasY - 10, 600);
   canvasFrame = createCanvas(canvasWidth, canvasHeight);
   canvasFrame.position(canvasX, canvasY);
   // set white background
@@ -131,9 +141,12 @@ function setup() {
   let clockMirror = 1;
   for (let i = 0; i < spaceInvaderRows; i++) {
     i % 2 == 0 ? xStart = spaceInvaderRadius : xStart = spaceInvaderColSpacing;
+    //let spaceInvaderR = random(255);
+    //let spaceInvaderG = random(255);
+    //let spaceInvaderB = random(255);
     let clockwise = clockMirror;
-    for (let x = xStart; x < (width - spaceInvaderRadius); x += spaceInvaderColSpacing) {
-      spaceInvaders.push(new SpaceInvader(imgs[i], x, y, clockwise * spaceInvaderAngle, spaceInvaderSpeed, 25, 0, 5));
+    for (let x = xStart; x < (width - spaceInvaderRadius + spaceInvaderColSpacing/2); x += spaceInvaderColSpacing) {
+      spaceInvaders.push(new SpaceInvader(imgs[i], x, y, spaceInvaderRadius, clockwise * spaceInvaderAngle, spaceInvaderSpeed, 25, 0, 5));//, spaceInvaderR, spaceInvaderG,spaceInvaderB));
       clockwise *= -1;
     }
     y += spaceInvaderRowHeight;
@@ -141,19 +154,21 @@ function setup() {
   }
   // create player's shooting slider
   shootSlider = createSlider(50, width - 50, width / 2, 0);
-  shootSlider.position(canvasX + 50, height - 20);//- 35);
+  shootSlider.position(canvasX + 50, height);//- 35);
   shootSlider.size(width - 100);
   shootSlider.input(shootRay);
+
   // create new game button
-  newGameButton = createButton('Rejouer');
-  newGameButton.position(canvasX + width/2 - 100, windowHeight - 40);
-  newGameButton.size(200,25);
-  newGameButton.style('font-family', 'Andika');
-  newGameButton.style('font-size', '12px');
-  newGameButton.mousePressed(newGame);
+    // create button for page reload / new tree
+    newGameButton = createButton('New Game');
+    newGameButton.position(canvasX + width/2 - 100, canvasY + height - 25);
+    newGameButton.size(200,25);
+    newGameButton.style('font-family', 'Andika');
+    newGameButton.style('font-size', '12px');
+    newGameButton.mousePressed(newGame);
 }
 // FUNCTIONS
-// shoot a ray when player moves shootSlider
+// shoot a ray when player moves shootSlider - for the time being, the bullets travel upwards (angle = -90)
 function shootRay() {
   if (rayBool % rayScarcity == 0) { 
     laserRays.push(new LaserRay(shootSlider.value(), height - 50, rayLength, -90, 5, random(255), random(255), random(255)));
@@ -169,10 +184,10 @@ function draw() {
   background(0);
   // finish line
   fill(255);
-  rect(-1, height - 90, width + 2, 2);
+  rect(-1, height - 95, width + 2, 2);
   // Nu "gun"
   imageNu.resize(50,0);
-  image(imageNu, shootSlider.value() - 23, height - 80);
+  image(imageNu, shootSlider.value() - 23, height - 90);//- 60);
   // show spaceInvaders
   if (spaceInvaders.length == 0) {
     noLoop();
@@ -188,7 +203,7 @@ function draw() {
       spaceInvaders[i].show();
       spaceInvaders[i].tick++;
       spaceInvaders[i].move();
-      if (spaceInvaders[i].y > height - spaceInvaderRadius - 50) {
+      if (spaceInvaders[i].y > height - 60 - spaceInvaderRadius) {
         noLoop();
         noStroke();
         fill(250);
@@ -199,7 +214,7 @@ function draw() {
         text("Oh nooon !\n ☠️Macron va gouverner pour toujours!☠️\n", width/2, height/2);
       }
     }
-    // shoot laserRays, pop spaceInvader out if hit x times and pop laserRay if out of frame
+    // shoot laserRays, pop spaceInvader out if hit and pop laserRay if out of frame
     for (let i = 0; i < laserRays.length; i++) {
       laserRays[i].show();
       laserRays[i].move();
